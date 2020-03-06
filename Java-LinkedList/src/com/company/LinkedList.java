10f54da1fc5e253eeb7c1f198357a93f4bcff29d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-06 22:50:53 +0330

    LinkedList

---
+++ Java-LinkedList/src/com/company/LinkedList.java
@@ -0,0 +1,177 @@
+package com.company;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    public class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+        this.value=value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    //isEmpty
+    private boolean isEmpty(){
+        return first==null;
+    }
+
+    //addFirst
+    public void addFirst(int item){
+        var node=new Node(item);
+        if(isEmpty())
+            first=last=node;
+        else {
+            node.next=first;
+            first=node;
+        }
+        size++;
+    }
+
+    //addLast
+    public void addLast(int item){
+        var node = new Node(item);
+        if(isEmpty())
+            first=last=node;
+        else {
+            last.next=node;
+            last=node;
+        }
+        size++;
+    }
+
+    //removeFirst
+    public void removeFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if(first==last)
+            first=last=null;
+        else{
+            var second=first.next;
+            first.next=null;
+            first=second;
+        }
+
+        size--;
+    }
+
+    //removeLast
+    public void removeLast(){
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(first==last)
+            first=last=null;
+        else {
+            var previous=getPrevious(last);
+            last=previous;
+            last.next=null;
+        }
+
+        size--;
+    }
+
+    private Node getPrevious(Node node){
+        var current=first;
+        while(current!=null){
+            if(current.next==node) return current;
+            current=current.next;
+        }
+        return null;
+    }
+
+    //contains
+    public boolean contains(int item){
+        return indexOf(item)!= -1;
+    }
+
+    //indexOf
+    public int indexOf(int item){
+        int index=0;
+        var current=first;
+        while(current!=null){
+            if (current.value==item)return index;
+            current=current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    //size
+    public int size(){
+        return  size;
+    }
+
+    //toArray
+    public int[] toArray(){
+        int[] array=new int[size];
+        var current=first;
+        var index=0;
+        while(current!=null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    //reverse
+    public void reverse(){
+        if(isEmpty())return;
+
+        var previous=first;
+        var current = first.next;
+        while(current!=null){
+            var next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
+        }
+
+        last=first;
+        last.next=null;
+        first=previous;
+    }
+
+    //get Kth
+    public int getKthFromTheEnd(int k){
+        if (isEmpty())
+            throw new IllegalStateException();
+        var a=first;
+        var b=first;
+        for(int i=0;i<k-1;i++){
+            b=b.next;
+            if(b==null)
+                throw new IllegalArgumentException();
+        }
+        while(b!=last){
+            a=a.next;
+            b=b.next;
+        }
+        return a.value;
+    }
+
+    //PrintMiddle
+    public void printMiddle(){
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var a=first;
+        var b=first;
+        while (b!=last && b.next!=last) {
+            b = b.next.next;
+            a = a.next;
+        }
+        if (b==last)
+            System.out.println(a.value);
+        else
+            System.out.println(a.value+", "+a.next.value);
+    }
+
+}
---
+++ Java-LinkedList/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+    var list =new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        list.printMiddle();
+
+
+        //System.out.println(list.getKthFromTheEnd(6 ));
+
+        //list.reverse();
+        //var array=list.toArray();
+        //System.out.println(Arrays.toString(array));
+
+        //System.out.println(list.size());
+
+        //list.removeFirst();
+        //list.removeLast();
+        //System.out.println(list.indexOf(20));
+        //System.out.println(list.contains(40));
+
+
+    }
+}
