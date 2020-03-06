bc3dd2e313ac41d8656055318b21524ede67708b
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-07 01:29:46 +0330

    PriorityQueue

---
+++ Java-PriorityQueue/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PriorityQueue queue=new PriorityQueue();
+        queue.add(5);
+        queue.add(3);
+        queue.add(6);
+        queue.add(1);
+        queue.add(4);
+        System.out.println(queue);
+
+        while (!queue.isEmpty())
+            System.out.println(queue.remove());
+    }
+}
