0a94ea74b80a751bb5622972f639d6b60d766346
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-09 22:21:02 +0330

    QueueWithTwoStacks

---
+++ Java-QueueWithTwoStacks/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QueueWithTwoStacks queue=new QueueWithTwoStacks();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        var first= queue.dequeue();
+        System.out.println(first);
+    }
+}
