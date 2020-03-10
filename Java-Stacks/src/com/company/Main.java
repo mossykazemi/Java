6ef86572b6bab36a020467020fd1e788e891fe00
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-10 19:22:31 +0330

    Stacks

---
+++ Java-Stacks/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//String str= "abcd";
+
+	//StringReverser reverser=new StringReverser();
+	//var result=reverser.reverse(str);
+    //System.out.println(result);
+
+    //===========================================
+
+        //Edge Cases
+        // (
+        // (()
+        // ) (
+        // ( ]
+
+     //String str= "(1 + 2)";
+     //Expression exp=new Expression();
+     //var result = exp.isBalanced(str);
+     //  System.out.println(result);
+
+    //========================================
+
+        Stack stack=new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
+    }
+}
